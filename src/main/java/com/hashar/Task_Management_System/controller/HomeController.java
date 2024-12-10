cc92fdbdb17754103ae0fa5604369a1913c99ef2
hashar <hashar@qburst.com>
2024-12-10 12:46:27 +0530

    Added project structure

---
+++ src/main/java/com/hashar/Task_Management_System/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.hashar.Task_Management_System.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(){
+        return new ResponseEntity<>("Hello welcome to Task Management System", HttpStatus.OK);
+    }
+}
