b81295422398b44395f5ab9bc62f0b9f027d5c3b
hashar <hashar@qburst.com>
2024-12-11 11:08:09 +0530

    Added user model

--- src/main/java/com/hashar/Task_Management_System/model/Member.java
+++ src/main/java/com/hashar/Task_Management_System/model/Member.java
@@ -1,19 +1,76 @@
 package com.hashar.Task_Management_System.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Data
+//@Getter
+//@Setter
+//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int member_id;
-    private String member_name;
-    private String email_id;
+    private int memberId;
+    private String memberName;
+    private String emailId;
     private String password;
     private String role;
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", memberName='" + memberName + '\'' +
+                ", emailId='" + emailId + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/hashar/Task_Management_System/repo/MemberRepo.java
+++ src/main/java/com/hashar/Task_Management_System/repo/MemberRepo.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface memberRepo extends JpaRepository<Member, Integer> {
+public interface MemberRepo extends JpaRepository<Member, Integer> {
+
+    Member findByMemberName(String memberName);
 }
